112fdb1e38422da8ad6cbbb0c1019c8db395fdcb
Jayneel Patel <jayneelp10@gmail.com>
2026-02-03 19:44:46 -0500

    initial sandbox file to mess around

---
+++ c++/sandbox.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int age = 20;
+    double gpa = 3.8;
+    char grade = 'A';
+    bool isStudent = true;
+
+    std::cout << "Age: " << age << std::endl;
+    std::cout << "GPA: " << gpa << std::endl;
+    std::cout << "Grade: " << grade << std::endl;
+    std::cout << "Is Student: " << isStudent << std::endl;
+    return 0;
+}
